7f74c7e8954220d516869439614f84810ec0a456
Dirk Surmann <surmann@statistik.tu-dortmund.de>
2018-03-27 14:59:35 +0200

    typos

--- .batchtools.conf.R
+++ .batchtools.conf.R
@@ -1,6 +1,6 @@
-luster.functions = makeClusterFunctionsSlurm("slurm-lido3")
+cluster.functions = makeClusterFunctionsSlurm("slurm-lido3")
 default.resources = list(  R = "R/3.4.3-gcc72-base"
-												 , measure.memory = TRUE
+                         , measure.memory = TRUE
                         )
 temp.dir = "~/nobackup/tmp"
 
